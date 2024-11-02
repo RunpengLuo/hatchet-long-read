03bd1f2dd92d60ffe146f56d9e5c741fb3f53bc0
John Luo <u7109933@anu.edu.au>
2024-11-02 02:55:01 -0400

    round totals to int

--- src/hatchet/utils/cluster_bins.py
+++ src/hatchet/utils/cluster_bins.py
@@ -217,8 +217,6 @@ def hmm_model_select(tracks, minK=20, maxK=50, tau=10e-6, tmat='diag', decode_al
     print(X)
     for i, track in enumerate(tracks):
         print(i, track)
-        if not all(np.isfinite(track)):
-            raise ValueError("Infinite")
 
     best_K = 0
     best_score = np.inf # BIC is always negative
