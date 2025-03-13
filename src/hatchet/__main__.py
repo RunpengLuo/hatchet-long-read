eb50bed4b8324ee244904e35bf5fb635fbac1dae
RunpengLuo <runpeng.luo@princeton.edu>
2025-03-13 15:27:32 -0400

    disable run_lr

--- src/hatchet/__main__.py
+++ src/hatchet/__main__.py
@@ -10,7 +10,7 @@ import hatchet
 from hatchet.utils.commands import commands, command_aliases
 
 # temporary options for Long read mode
-from hatchet.utils.run_lr import main as run_lr  # noqa: F401
+# from hatchet.utils.run_lr import main as run_lr  # noqa: F401
 from hatchet.utils.count_reads_lr import main as count_reads_lr # noqa: F401
 from hatchet.utils.combine_counts_lr2 import main as combine_counts_lr # noqa: F401
 from hatchet.utils.genotype_snps_lr import main as genotype_snps_lr # noqa: F401
