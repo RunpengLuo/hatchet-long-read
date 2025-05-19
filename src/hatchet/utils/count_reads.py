ec61004ffc22619c6ba14a697bc6aee7cb350dff
RunpengLuo <runpeng.luo@princeton.edu>
2025-05-19 10:28:54 -0400

    fix index0 error when threshold_p is empty

--- src/hatchet/utils/count_reads.py
+++ src/hatchet/utils/count_reads.py
@@ -555,12 +555,11 @@ def run_chromosome(
             thresholds_p = np.trunc(
             np.vstack([positions_p[:-1], positions_p[1:]]).mean(axis=0)
             ).astype(np.uint32)
+            if thresholds_p[0] != 1:
+                thresholds_p = np.concatenate([[1], thresholds_p])
         else:
             log(msg=f"WARNING, no SNPs are found for {chromosome} p-arm\n", level="WARN")
             thresholds_p = np.array([], dtype=np.int32)
-
-        if thresholds_p[0] != 1:
-            thresholds_p = np.concatenate([[1], thresholds_p])
 
         snp_indices_q_arm = np.where(positions > centromere_end)[0]
         if len(snp_indices_q_arm) > 0:
